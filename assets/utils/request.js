cfcc1b767cc786befe898f18ca36e0d90a90d71a
CoderQiQin521 <coderqiqin@aliyun.com>
2019-12-20 10:37:43 +0800

    request

--- assets/utils/request.js
+++ assets/utils/request.js
@@ -1,9 +1,8 @@
 import axios from '@/js_sdk/xtshadow-axios/axios.min'
-
-axios.get('https://api.github.com').then(res => {
-  console.log(res.data);
-
+// 插件地址 https://ext.dcloud.net.cn/plugin?id=1088
+const instance = axios.create({
+  baseURL: '',
+  timeout: 6000
 })
 
-
-export default axios
+export default instance
